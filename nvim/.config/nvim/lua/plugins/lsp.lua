9bb81983a91a35c1db089a016f2faaa771c36754
RockingMonki <thistovivek@gmail.com>
2026-01-25 12:10:06 +0530

    some changes

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,34 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependecies = {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  },
+  config = function()
+    local capabilities = {
+      textDocumnet = {
+        foldingRange = {
+          dynamicRegistration = false,
+          lineFoldingOnly = true
+        }
+      }
+    }
+
+    capabilities = require('blink.cmp').get_lsp_capabilities(capabilites)
+
+    vim.lsp.config('lua_ls', {
+      capabilities = capabilites,
+    })
+
+    vim.lsp.enable({
+      'lua_ls'
+    })
+  end,
+}
