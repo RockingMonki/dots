fdec5ed1a2933911e915d5df6f77e80b6a40ce42
RockingMonki <vgsr65@gmail.com>
2026-03-22 19:59:42 +0530

    initial setup

---
+++ nvim/.config/nvim/lua/config/mappings.lua
@@ -0,0 +1,3 @@
+local map = vim.keymap.set 
+
+map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit", silent = true })
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+local opt = vim.opt 
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+opt.number = true
+opt.relativenumber = true
+opt.cmdheight = 0
