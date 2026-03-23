055f2e67e7650d29d3e7b947fe70b8fd04542046
RockingMonki <vgsr65@gmail.com>
2026-03-23 17:32:13 +0530

    reasonable config

--- nvim/.config/nvim/lua/config/mappings.lua
+++ nvim/.config/nvim/lua/config/mappings.lua
@@ -2,3 +2,9 @@ local map = vim.keymap.set
 
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit", silent = true })
 map("n", "<leader>w", "<cmd>w<cr>", { desc = "Write", silent = true })
+
+-- buffer management
+map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Close buf", silent = true })
+map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next buf", silent = true })
+map("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Prev buf", silent = true })
+
